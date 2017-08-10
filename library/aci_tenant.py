388b50f9020e35fffd63347132b327f0f9583839
Dag Wieers <dag@wieers.com>
2017-08-11 00:34:33 +0200

    Updated aci_tenant (#4)
    * Updated aci_tenant
    
    * Small fix
    
    * One typo

--- library/aci_tenant.py
+++ library/aci_tenant.py
@@ -1,25 +1,16 @@
-#!usr/bin/python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: aci_tenant
 short_description: Manage tenants on Cisco ACI fabrics
@@ -32,29 +23,32 @@ version_added: '2.4'
 requirements:
 - ACI Fabric 1.0(3f)+
 options:
-  tenant_name:
+  tenant:
     description:
     - The name of the tenant.
     required: yes
-  descr:
+    aliases: [ name, tenant_name ]
+  description:
     description:
     - Description for the AEP.
+    aliases: [ descr ]
   state:
     description:
-    - present, absent, query
-    default: present
+    - Use C(present) or C(absent) for adding or removing.
+    - Use C(query) for listing an object or multiple objects.
     choices: [ absent, present, query ]
+    default: present
 extends_documentation_fragment: aci
 '''
 
-EXAMPLES = '''
+EXAMPLES = r'''
 - name: Add a new tenant
   aci_tenant:
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of the tenant
-    description: Description for the tenant
+    tenant: production
+    description: Production tenant
     state: present
 
 - name: Remove a tenant
@@ -62,7 +56,7 @@ EXAMPLES = '''
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of the tenant
+    tenant: production
     state: absent
 
 - name: Query a tenant
@@ -70,7 +64,7 @@ EXAMPLES = '''
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of the tenant
+    tenant: production
     state: query
 
 - name: Query all tenants
@@ -81,14 +75,14 @@ EXAMPLES = '''
     state: query
 '''
 
-RETURN = '''
+RETURN = r'''
 status:
-  description: The status code of the http request
-  returned: upon making a successful GET, POST or DELETE request to the APIC
+  description: status code of the http request
+  returned: always
   type: int
   sample: 200
 response:
-  description: Response text returned by APIC
+  description: response text returned by APIC
   returned: when a HTTP request has been made to APIC
   type: string
   sample: '{"totalCount":"0","imdata":[]}'
@@ -101,7 +95,7 @@ from ansible.module_utils.basic import AnsibleModule
 def main():
     argument_spec = aci_argument_spec
     argument_spec.update(
-        tenant_name=dict(type='str', aliases=['name'], required=False),
+        tenant=dict(type='str', aliases=['name', 'tenant_name'], required=False),
         description=dict(type='str', aliases=['descr'], required=False),
         state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
         method=dict(type='str', choices=['delete', 'get', 'post'], aliases=['action'], removed_in_version='2.6'),  # Deprecated starting from v2.6
@@ -112,33 +106,33 @@ def main():
         supports_check_mode=True,
     )
 
-    tenant_name = module.params['tenant_name']
+    tenant = module.params['tenant']
     description = module.params['description']
     state = module.params['state']
 
     aci = ACIModule(module)
 
-    if tenant_name is not None:
+    if tenant is not None:
         # Work with a specific tenant
-        path = 'api/mo/uni/tn-%(tenant_name)s.json' % module.params
+        path = 'api/mo/uni/tn-%(tenant)s.json' % module.params
     elif state == 'query':
         # Query all tenants
         path = 'api/class/fvTenant.json'
     else:
-        module.fail_json(msg="Parameter 'tenant_name' is required for state 'absent' or 'present'")
+        module.fail_json(msg="Parameter 'tenant' is required for state 'absent' or 'present'")
 
     aci.result['url'] = '%(protocol)s://%(hostname)s/' % aci.params + path
 
     aci.get_existing()
 
     if state == 'present':
-        # Filter out module params with null values
-        aci.payload(aci_class='fvTenant', class_config=dict(name=tenant_name, descr=description))
+        # Filter out module parameters with null values
+        aci.payload(aci_class='fvTenant', class_config=dict(name=tenant, descr=description))
 
-        # generate config diff which will be used as POST reqest body
+        # Generate config diff which will be used as POST request body
         aci.get_diff(aci_class='fvTenant')
 
-        # submit changes if module not in check_mode and the proposed is different than existing
+        # Submit changes if module not in check_mode and the proposed is different than existing
         aci.post_config()
 
     elif state == 'absent':
