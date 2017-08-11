e957c8299921adddcd9a50a41ae0508247a5bb3d
Dag Wieers <dag@wieers.com>
2017-08-11 02:45:29 +0200

    aci_filter: Refactor (#5)
    * Refactor aci_filter
    
    * Improved parameters list
    
    * Switch name alias !
    
    * Empty return value
    
    * Add missing state parameter

--- library/aci_filter.py
+++ library/aci_filter.py
@@ -1,175 +1,150 @@
 #!/usr/bin/python
-DOCUMENTATION = '''
----
+# -*- coding: utf-8 -*-
 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
 module: aci_filter
 short_description: Manages top level filter objects
 description:
-    -  Manages top level filter objects, i.e. not each entry
-author: Cisco
+- Manages top level filter objects, i.e. not each entry
+author:
+- Swetha Chunduri (@schunduri)
+- Dag Wieers (@dagwieers)
+- Jacob McGill (@jmcgill298)
+version_added: '2.4'
 requirements:
-    - ACI Fabric 1.0(3f)+
-notes: Tenant must be exist prior to using this module
+- ACI Fabric 1.0(3f)+
+notes:
+- The tenant used must exist before using this module in your playbook. The M(aci_tenant) module can be used for this.
 options:
-    action:
-        description:
-            - post, get, or delete
-        required: true
-        default: null
-        choices: ['post','get', 'delete']
-        aliases: []
-    tenant_name:
-        description:
-            - Tenant Name
-        required: true
-        default: null
-        choices: []
-        aliases: []
-   filter_name:
-        description:
-            -Filter Name
-        required: true
-        default: null
-        choices: []
-        aliases: []
-     descr:
-        description:
-            - Description for the AEP
-        required: false
-        default: null
-        choices: []
-        aliases: []
-    host:
-        description:
-            - IP Address or hostname of APIC resolvable by Ansible control host
-        required: true
-        default: null
-        choices: []
-        aliases: []
-    username:
-        description:
-            - Username used to login to the switch
-        required: true
-        default: 'admin'
-        choices: []
-        aliases: []
-    password:
-        description:
-            - Password used to login to the switch
-        required: true
-        default: null
-        choices: []
-        aliases:[]                                                                  
-    protocol:
-        description:
-            - Dictates connection protocol to use
-        required: false
-        default: https
-        choices: ['http', 'https']
-        aliases: []
+  filter:
+    description:
+    - The name of the filter.
+    required: yes
+    aliases: [ filter_name, name ]
+  description:
+    description:
+    - Description for the filter.
+    aliases: [ descr ]
+  tenant:
+    description:
+    - The name of the tenant.
+    required: yes
+    aliases: [ tenant_name ]
+  state:
+    description:
+    - Use C(present) or C(absent) for adding or removing.
+    - Use C(query) for listing an object or multiple objects.
+    choices: [ absent, present, query ]
+    default: present
+extends_documentation_fragment: aci
 '''
 
-EXAMPLES = '''
+EXAMPLES = r'''
+- name: Add a new filter to a tenant
+  aci_filter:
+    hostname: apic
+    username: admin
+    password: SomeSecretPassword
+    filter: web_filter
+    description: Filter for web protocols
+    tenant: production
+    state: present
 
-    aci_filter:
-       action: "{{ action }}"
-       filter_name: "{{ filter_name }}"
-       tenant_name: "{{ tenant_name }}"
-       descr: "{{ descr }}"
-       host: "{{ inventory_hostname }}"
-       username: "{{ user }}"
-       password: "{{ pass }}"
-       protocol: "{{ protocol }}"
+- name: Remove a filter for a tenant
+  aci_filter:
+    hostname: apic
+    username: admin
+    password: SomeSecretPassword
+    filter: web_filter
+    tenant: production
+    state: absent
+  
+- name: Query a filter of a tenant
+  aci_filter:
+    hostname: apic
+    username: admin
+    password: SomeSecretPassword
+    filter: web_filter
+    tenant: production
+    state: query
 
+- name: Query all filters for a tenant
+  aci_filter:
+    hostname: apic
+    username: admin
+    password: SomeSecretPassword
+    tenant: production
+    state: query
 '''
 
+RETURN = r'''
+#
+'''
 
-import socket
-import json
-import requests
+from ansible.module_utils.aci import ACIModule, aci_argument_spec
+from ansible.module_utils.basic import AnsibleModule
+
 
 def main():
-
-    ''' Ansible module to take all the parameter values from the playbook '''
-    module = AnsibleModule(
-        argument_spec=dict(
-            action=dict(choices=['post', 'get', 'delete']),
-            host=dict(required=True),
-            username=dict(type='str', default='admin'),
-            password=dict(type='str'),
-            protocol=dict(choices=['http', 'https'], default='https'),
-            filter_name=dict(type="str", required=False),
-            tenant_name=dict(type="str", required=False),
-            descr=dict(type="sitr", required=False),
-        ),
-        supports_check_mode=False
+    argument_spec = aci_argument_spec
+    argument_spec.update(
+        filter=dict(type='str', required=False, aliases=['name', 'filter_name']),  # Not required for querying all filters
+        tenant=dict(type='str', required=True, aliases=['tenant_name']),
+        description=dict(type='str', aliases=['descr']),
+        state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
+        method=dict(type='str', choices=['delete', 'get', 'post'], aliases=['action'], removed_in_version='2.6'),  # Deprecated starting from v2.6
     )
 
-    filter_name=module.params['filter_name']
-    tenant_name=module.params['tenant_name']
-    descr=module.params['descr']
-    descr=str(descr)
-    username = module.params['username']
-    password = module.params['password']
-    protocol = module.params['protocol']
-    host = socket.gethostbyname(module.params['host'])
-    action = module.params['action']
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
 
+    filter_name = module.params['filter']
+    tenant = module.params['tenant']
+    description = module.params['description']
+    state = module.params['state']
 
-    post_uri ='api/mo/uni/tn-'+tenant_name+'/flt-'+filter_name+'.json'
-    get_uri = 'api/node/class/vzFilter.json'
+    aci = ACIModule(module)
 
-    ''' Config payload to enable the physical interface '''
-    config_data = {"vzFilter":{"attributes":{"name": filter_name, "descr":descr}}}
-    payload_data = json.dumps(config_data)
-
-    ''' authentication || || Throw an error otherwise'''
-    apic = '{0}://{1}/'.format(protocol, host)
-    auth = dict(aaaUser=dict(attributes=dict(name=username, pwd=password)))
-    url=apic+'api/aaaLogin.json'
-    authenticate = requests.post(url, data=json.dumps(auth), timeout=2, verify=False)
-
-    if authenticate.status_code != 200:
-        module.fail_json(msg='could not authenticate to apic', status=authenticate.status_code, response=authenticate.text)
-
-    ''' Sending the request to APIC '''
-    if post_uri.startswith('/'):
-        post_uri = post_uri[1:]
-    post_url = apic + post_uri
-
-    if get_uri.startswith('/'):
-        get_uri = get_uri[1:]
-    get_url = apic + get_uri
-
-    if action == 'post':
-        req = requests.post(post_url, cookies=authenticate.cookies, data=payload_data, verify=False)
-
-    elif action == 'get':
-        req = requests.get(get_url, cookies=authenticate.cookies, data=payload_data, verify=False)
-   
-    elif action == 'delete':
-        req = requests.delete(post_url, cookies=authenticate.cookies, data=payload_data, verify=False)
-
-    ''' Check response status and parse it for status || Throw an error otherwise '''
-    response = req.text
-    status = req.status_code
-    changed = False
-
-    if req.status_code == 200:
-        if action == 'post':
-            changed = True
-        else:
-            changed = False
-
+    if filter_name is not None:
+        # Work with a specific filter
+        path = 'api/mo/uni/tn-%(tenant)s/flt-%(filter_name)s.json' % module.params
+    elif state == 'query':
+        # Query all filters
+        path = 'api/node/class/vzFilter.json'
     else:
-        module.fail_json(msg='error issuing api request',response=response, status=status)
+        module.fail_json(msg="Parameter 'filter' is required for state 'absent' or 'present'")
 
-    results = {}
-    results['status'] = status
-    results['response'] = response
-    results['changed'] = changed
-    module.exit_json(**results)
+    aci.result['url'] = '%(protocol)s://%(hostname)s/' % aci.params + path
 
-from ansible.module_utils.basic import *
+    aci.get_existing()
+
+    if state == 'present':
+        # Filter out module parameters with null values
+        aci.payload(aci_class='vzFilter', class_config=dict(name=filter_name, descr=description))
+
+        # Generate config diff which will be used as POST request body
+        aci.get_diff(aci_class='vzFilter')
+
+        # Submit changes if module not in check_mode and the proposed is different than existing
+        aci.post_config()
+
+    elif state == 'absent':
+        aci.delete_config()
+
+    module.exit_json(**aci.result)
+
+
 if __name__ == "__main__":
     main()
